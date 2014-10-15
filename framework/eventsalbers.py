52b7e36a04697fb3d85c68b030462ad05f203082
Colin <colin.bernet@cern.ch>
2014-10-15 12:53:06 +0200

    removed core stuff

--- framework/eventsalbers.py
+++
@@ -1,9 +0,0 @@
-from eventstore import EventStore as Events
-
-## class Events(object):
-
-##     def __init__(self, files, tree_name):
-##         self.store = EventStore(files[0])
-
-## #    def __len__(self):
-## #        return len(self.store)
