2926516fadda6e744ef5ea3f99752d3d93daf914
Colin <colin.bernet@cern.ch>
2015-02-19 15:25:16 +0100

    more direct cfg for display

---
+++ test/pfsim_cfg.py
@@ -0,0 +1,60 @@
+import os
+import heppy.framework.config as cfg
+import logging
+logging.basicConfig(level=logging.WARNING)
+
+# input component 
+# several input components can be declared,
+# and added to the list of selected components
+inputSample = cfg.Component(
+    'albers_example',
+    files = ['example.root'],
+    )
+
+selectedComponents  = [inputSample]
+
+
+from heppy_fcc.analyzers.PFSim import PFSim
+pfsim = cfg.Analyzer(
+    PFSim,
+    verbose = True
+)
+
+# definition of a sequence of analyzers,
+# the analyzers will process each event in this order
+sequence = cfg.Sequence( [
+    pfsim,
+    ] )
+
+# inputSample.files.append('albers_2.root')
+# inputSample.splitFactor = 2  # splitting the component in 2 chunks
+
+# finalization of the configuration object.
+from ROOT import gSystem
+gSystem.Load("libdatamodel")
+from eventstore import EventStore as Events
+config = cfg.Config(
+    components = selectedComponents,
+    sequence = sequence,
+    services = [],
+    events_class = Events
+)
+
+def process(iev):
+    loop.process(iev)
+    display.draw()
+
+if __name__ == '__main__':
+    from heppy.framework.looper import Looper
+    import sys
+
+    iev = int(sys.argv[1])
+    loop = Looper( 'looper',
+                   config,
+                   1000, 0,
+                   nPrint = 0,
+                   timeReport = False,
+                   quiet = True)
+
+    display = loop.analyzers[0].display
+    process(iev)
