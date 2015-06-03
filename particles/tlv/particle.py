c76cd0dd3db442be547429ab52fbdc7e4df5848a
Colin <colin.bernet@cern.ch>
2015-06-03 14:17:42 +0200

    status added to tlv particles, as it is now needed in analysers

--- particles/tlv/particle.py
+++ particles/tlv/particle.py
@@ -3,7 +3,8 @@ from heppy_fcc.particles.particle import Particle as BaseParticle
 import math
 
 class Particle(BaseParticle):
-    def __init__(self, pdgid, charge, tlv):
+    def __init__(self, pdgid, charge, tlv, status=1):
         self._pid = pdgid
         self._charge = charge
         self._tlv = tlv
+        self._status = status
