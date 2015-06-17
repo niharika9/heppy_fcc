c7077ee00e708a8d7d9afb0977ae6a5ff4dcc022
Colin <colin.bernet@cern.ch>
2015-06-17 12:34:53 +0200

    macro for particle efficiency studies

---
+++ test/particle_efficiency.py
@@ -0,0 +1,96 @@
+from cpyroot import *
+from OfficialStyle import *
+
+import sys 
+
+
+class Efficiency(object):
+
+    def __init__(self, name, tree):
+        self.name = name
+        self.tree = tree
+        self.num = None
+        self.denom = None
+        self.eff = None
+        
+    def hname(self, name):
+        return '_'.join([self.name, name])
+        
+    def project(self, var, cut, numcut, nbins, xmin, xmax):
+        if cut == '':
+            cut = '1'
+        if self.num is None: 
+            self.num = TH1F( self.hname('num'), self.name, nbins, xmin, xmax)
+        else:
+            self.num.Reset()
+        self.num.Sumw2()
+        sRed.formatHisto(self.num)
+        if self.denom is None: 
+            self.denom = TH1F( self.hname('denom'), self.name, nbins, xmin, xmax)
+        else:
+            self.denom.Reset()
+        self.denom.Sumw2()
+        sBlue.formatHisto(self.num)
+        if self.eff is None: 
+            self.eff = TH1F( self.hname('eff'), self.name, nbins, xmin, xmax)
+        else:
+            self.eff.Reset()
+        self.eff.Sumw2()
+        hists = [self.num, self.denom, self.eff]
+        for h in hists:
+            h.SetTitle(self.name)
+            h.GetXaxis().SetTitle(var)
+            h.GetYaxis().SetTitle('efficiency')
+        
+        print 'denom: var = ', var, ' cut = ', cut
+        self.tree.Project(self.denom.GetName(), var, cut)
+        numcut = '({cut}) && ({numcut})'.format(cut=cut,
+                                                numcut=numcut)
+        print 'num: var = ', var, ' cut = ', numcut
+        self.tree.Project(self.num.GetName(), var, numcut)
+        self.eff.Divide(self.num, self.denom, 1, 1, 'B')
+
+    
+        
+officialStyle(gStyle)
+
+sample = sys.argv[1]
+
+papas_rootfile = 'heppy_fcc.analyzers.ParticleTreeProducer.ParticleTreeProducer_papas_g2r/tree.root'
+cms_rootfile = 'heppy_fcc.analyzers.ParticleTreeProducer.ParticleTreeProducer_cms_g2r/tree.root'
+
+papas_rootfile = '/'.join([sample, papas_rootfile])
+cms_rootfile = '/'.join([sample, cms_rootfile])
+
+papas = Chain(papas_rootfile)
+cms = Chain(cms_rootfile)
+
+
+def efficiencies(name, tree):
+    
+    e_eff = Efficiency('_'.join([name, 'e']), tree)
+    e_eff.project('ptc_e', '', 'ptc_match_e>0.', 20, 0, 10)
+    e_eff.eff.Draw()
+
+    # pt_eff = Efficiency('_'.join([name, 'pt']), tree)
+    # pt_eff.project('ptc_pt', '', 'ptc_match_e>0.', 20, 0, 10)
+    # pt_eff.eff.Draw()
+
+    eta_eff = Efficiency('_'.join([name, 'eta']), tree)
+    eta_eff.project('ptc_eta', 'ptc_e>3.', 'ptc_match_e>0.', 20, -1.5, 1.5)
+    eta_eff.eff.Draw()
+
+    return [e_eff, eta_eff]
+
+papas_eff = efficiencies('papas', papas)
+cms_eff = efficiencies('cms', cms)
+
+c1 = TCanvas()
+papas_eff[0].eff.Draw()
+sBlue.formatHisto(cms_eff[0].eff)
+cms_eff[0].eff.Draw('same')
+
+c2 = TCanvas()
+papas_eff[1].eff.Draw()
+sBlue.formatHisto(cms_eff[0].eff)
+cms_eff[1].eff.Draw('same')
